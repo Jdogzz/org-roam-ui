aa9d5bf879a0e7e78e59c83bd3b3d63e83c058ed
Jdogzz <2792697+Jdogzz@users.noreply.github.com>
2025-12-15 21:19:44 -0800

    Update org-roam-ui.el
    Switch to 0.0.0.0

--- org-roam-ui.el
+++ org-roam-ui.el
@@ -186,7 +186,7 @@ This serves the web-build and API over HTTP."
     (setq org-roam-ui-ws-server
           (websocket-server
            35903
-           :host 'local
+           :host "0.0.0.0"
            :on-open #'org-roam-ui--ws-on-open
            :on-message #'org-roam-ui--ws-on-message
            :on-close #'org-roam-ui--ws-on-close))
